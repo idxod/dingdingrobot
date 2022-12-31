7fd0d7f61bc75f36a0fb844747ef1dd1dacbf52d
阿甘 <39944530+idxod@users.noreply.github.com>
2022-12-31 17:12:52 +0800

    20221231
    修正了自己的app_key
    修正了自己的app_secret

--- settings.example.py
+++ settings.example.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 app_settings = {
-    "cookie_secret": "",
+    "cookie_secret": "anyappcookiesecret",
     "dt_app_info": {
-        "app_key": "your-app-key",
-        "app_secret": "your-app-secret",
+        "app_key": "dingfga4axrwkc4xwvv8",
+        "app_secret": "jJxwX57lWloM4MPSmzqA_xIKP9j3uf1-hIdwmBP6fREirEqj_hKmv_EwaNqGD58M",
     },
     "dt_robot_info": {
-        "code": "your-robot-code",
+        "code": "dingfga4axrwkc4xwvv8",
     },
     "dt_cool_app_info_001": {
-        "code": "your-cool-app-code",
+        "code": "COOLAPP-1-1020350FA47E2133EF14000G",
     },
     "dt_interactive_cards": {
-        "message_card_template_id_001": "your-message-card-template-id",
-        "top_card_template_id_001": "your-top-card-template-id",
+        "message_card_template_id_001": "dde980a2-fb8a-446e-9b72-70c45317b076",
+        "top_card_template_id_001": "e03f3551-f3b4-4a0d-8183-981ee70b1b41",
     },
 }
